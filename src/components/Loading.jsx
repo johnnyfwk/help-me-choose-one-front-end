4118e4cb254ae0c72113eb7c5ac1f4588b054de2
johnnyfwk <johnnyfong@live.com>
2023-08-01 08:43:24 +0100

    Removed dad jokes from 'Loading' component.

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,33 +1,33 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
 
 export default function Loading() {
-    const [joke, setJoke] = useState("");
+    // const [joke, setJoke] = useState("");
 
-    const onClickNextJokeButton = async () => {
-        try {
-            const response = await axios.get('https://icanhazdadjoke.com/', {
-                headers: { Accept: 'application/json' },
-            });
-            setJoke(response.data.joke);
-            } catch (error) {
-                console.error('Error fetching dad joke:', error);
-                setJoke("Couldn't get dad joke.")
-            }
-    };
+    // const onClickNextJokeButton = async () => {
+    //     try {
+    //         const response = await axios.get('https://icanhazdadjoke.com/', {
+    //             headers: { Accept: 'application/json' },
+    //         });
+    //         setJoke(response.data.joke);
+    //         } catch (error) {
+    //             console.error('Error fetching dad joke:', error);
+    //             setJoke("Couldn't get dad joke.")
+    //         }
+    // };
 
-    useEffect(() => {
-        onClickNextJokeButton();
-    }, [])
+    // useEffect(() => {
+    //     onClickNextJokeButton();
+    // }, [])
 
     return (
         <main>
             <p>Page is loading...</p>
-            <p>While you're waiting, here are some dad jokes to keep you entertained.</p>
+            {/* <p>While you're waiting, here are some dad jokes to keep you entertained.</p>
             <p id="loading-joke">{joke}</p>
             <div>
                 <button type="button" onClick={onClickNextJokeButton}>Next Joke</button>
-            </div>
+            </div> */}
         </main>
     )
 }
